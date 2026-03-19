5a0bec8972a574d293325f45032c2e34739016b0
Rivia <armaanahmed413@gmail.com>
2026-03-19 14:31:43 +0000

    vault backup: 2026-03-19 14:31:43

--- Programming Languages and Frameworks/Haskell/Snippets/Type Constructors.hs
+++ Programming Languages and Frameworks/Haskell/Snippets/Type Constructors.hs
@@ -1,5 +1,8 @@
+-- TYPE LEVEL: We use the Type Constructor (Maybe) to define the contract
 findUser :: Int -> Maybe String
-
-findUser 1 = Just "alice"
-findUser 2 = Just "bob"
+-- VALUE LEVEL: We use the Data Constructors (Just, Nothing) to build the data
+findUser 1 = Just "Alice"
+findUser 2 = Just "Bob"
 findUser _ = Nothing
+
+main = print (findUser 3)
