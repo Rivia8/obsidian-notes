6d0cff7c7f330b3d8fb67d684bd725d915a24e96
Armaan Ahmed <armaan.ahmed@student.manchester.ac.uk>
2026-03-30 20:15:11 +0000

    vault backup: 2026-03-30 21:15:11

--- Programming Languages and Frameworks/C++/Snippets/MyStack.cpp
+++ Programming Languages and Frameworks/C++/Snippets/MyStack.cpp
@@ -1,4 +1,4 @@
-// Generic data Structure: Stack
+// Generic data Structure: vec
 // LIFO container
 
 #include <stdexcept>
@@ -10,23 +10,23 @@ template <typename T>
 class MyStack{
 
     private:
-        std::vector<T> stack; // Standard vector to focus on logic and not the sizing.
+        std::vector<T> vec; // Standard vector to focus on logic and not the sizing.
 
     void push(T item) {
-
+        this->vec.push_back(item);
     }
 
     bool isEmpty(){
         // .empty() returns 1 if the vector is empty and 0 otherwise
-        return this->stack.empty();
+        return this->vec.empty();
     }
 
     T peek(){
-        // Returns the item at the top of the stack WITHOUT REMOVING it from the internal vector.
+        // Returns the item at the top of the vector WITHOUT REMOVING it from the internal vector.
         
         try {
-            return this->stack[0];
-            throw std::out_of_range("The stack is empty"); 
+            return this->vec[((this->vec.size()) - 1)];
+            throw std::out_of_range("The vec is empty"); 
         }
         catch (int errorCode) {
             std::cout << "Error has Occurred: " << errorCode;
@@ -34,16 +34,30 @@ class MyStack{
     }
 
     T pop(){
-        // Returns hte item at the top of the stack and REMOVES it from the internal vecotr.
+        // Returns hte item at the top of the vector and REMOVES it from the internal vecotr.
         try {
-            int pos = (this->stack.size()) - 1;
-            T element = this->stack[pos];
+            int pos = (this->vec.size()) - 1;
+            T element = this->vec[pos];
+            this->vec.erase(pos); // Erase resizes.
             return element;
-            throw std::out_of_range("The stack is empty"); 
+            throw std::out_of_range("The vec is empty"); 
         }
         catch (int errorCode) {
             std::cout << "Error has Occurred: " << errorCode;
         }
+    }
+
+    int main(){
+        MyStack<int> MyStack;
+        MyStack.push(10);
+        MyStack.push(20);
+        MyStack.push(30);
+
+        while (!MyStack.isEmpty()){
+            std::cout << "item: " << MyStack.pop();
+        }
+        
+        return 0;
     }
 
 };
